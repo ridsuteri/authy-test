5bbd91c8007b66966c95e8e8db5bf75091ad064e
Riddhi Suteri <ridsuteri@gmail.com>
2022-12-08 11:44:20 +0530

    Feat: add/remove users to an organisation

--- controllers/orgsController.js
+++ controllers/orgsController.js
@@ -39,3 +39,88 @@ module.exports.addOrg = (req, res) => {
     }
 }
 
+// update an organization
+module.exports.updateOrg = (req, res) => {
+    if (req.user.isAdmin == 1) {
+        let org = req.body;
+        var sql = "UPDATE organizations SET regId = ?, name = ?, category = ? WHERE user_id = ?";
+        mysqlConnection.query(sql, [org.regId, org.name, org.category, org.user_id], (err, rows, fields) => {
+            if (!err)
+                res.send('Updated organization id: ' + req.params.id);
+            else
+                console.log(err);
+        })
+    } else {
+        res.send('You do not have permission to update an organization');
+    }
+}
+
+// delete an organization
+module.exports.deleteOrg = (req, res) => {
+    if (req.user.isAdmin == 1) {
+        mysqlConnection.query('DELETE FROM organizations WHERE regId = ?', [req.params.id], (err, rows, fields) => {
+            if (!err)
+                res.send('Deleted organization id: ' + req.params.id);
+            else
+                console.log(err);
+        })
+    } else {
+        res.send('You do not have permission to delete an organization');
+    }
+}
+
+// view all organizations by user id
+module.exports.viewOrgsByUser = (req, res) => {
+    mysqlConnection.query('SELECT * FROM organizations WHERE user_id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+}
+
+// allow users to add new users to their organization if they are an admin
+module.exports.addUserToOrg = (req, res) => {
+    let org = req.body;
+    if (req.user.isAdmin == 1) {
+        var sql = "INSERT INTO organizations (user_id, organization_id, role) VALUES (?, ?, ?)";
+        mysqlConnection.query(sql, [org.user_id, org.organization_id, org.role], (err, rows, fields) => {
+            if (!err)
+                rows.forEach(element => {
+                    if (element.constructor == Array)
+                        res.send('Inserted user id: ' + element[0].user_id);
+                });
+            else
+                console.log(err);
+        })
+    } else {
+        res.send('You do not have permission to add users to this organization');
+    }
+}
+
+// allow users to remove users from their organization if they are an admin
+module.exports.removeUserFromOrg = (req, res) => {
+    let org = req.body;
+    if (req.user.isAdmin == 1) {
+        mysqlConnection.query('DELETE FROM organizations WHERE user_id = ? AND organization_id = ?', [org.user_id, org.organization_id], (err, rows, fields) => {
+            if (!err)
+                res.send('Deleted user id: ' + org.user_id);
+            else
+                console.log(err);
+        })
+    } else {
+        res.send('You do not have permission to remove users from this organization');
+    }
+}
+
+
+// allow users to view all users in their organization
+module.exports.viewUsersInOrg = (req, res) => {
+    mysqlConnection.query('SELECT * FROM organizations WHERE organization_id = ?', [req.params.id], (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+}
+
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const Router = express.Router();
 
-const { viewAllUsers } = require('../controllers/usersController');
+const { viewAllUsers, updateOrg, deleteOrg, viewOrgsByUser } = require('../controllers/usersController');
+// managing users in an organization
+const { addUserToOrg, removeUserFromOrg, viewUsersInOrg } = require('../controllers/orgsController');
 
 Router.get('/all', viewAllUsers);
+Router.put('/update/:id', updateOrg);
+Router.delete('/delete/:id', deleteOrg);
+Router.get('/user/:id', viewOrgsByUser);
+
+// routes for allowing user to add new users to an organization
+Router.post('/addUserToOrg', addUserToOrg);
+Router.delete('/removeUserFromOrg', removeUserFromOrg);
+Router.get('/viewUsersInOrg/:id', viewUsersInOrg);
 
 module.exports = Router;
